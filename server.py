5cf5135b2c108f9e3a2764fa1262781cb97ff2ff
oleska <oleska1601@gmail.com>
2023-03-14 13:55:06 +0300

    add files

--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 # импортируем библиотеки
+import os
 from flask import Flask, request, jsonify
 import logging
 
@@ -128,4 +129,5 @@ def get_suggests(user_id):
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
